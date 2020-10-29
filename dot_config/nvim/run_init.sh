2c30535bc4f4dc0a995386278f7538fcd1f0c8f3
blue <blue@sourcecode.li>
2020-10-29 10:13:00 +0100

    Change order of plug install for vim

--- dot_config/nvim/run_init.sh
+++ dot_config/nvim/run_init.sh
@@ -2,6 +2,9 @@
 
 echo "install PlugManger"
 
+echo "install python package for neovim"
+pip install --user neovim
+
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo "install all plugins"
@@ -10,5 +13,3 @@ nvim '+ PlugInstall --sync' +qall &> /dev/null < /dev/tty
 echo "Update plugin - syntax highlight for python"
 nvim +UpdateRemotePlugins +qall
 
-echo "install python package for neovim"
-pip install --user neovim
